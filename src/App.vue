28bc24742f4150d8873703f800a4c7aa6720b097
Mozumi Issei <19aw0123@jec.ac.jp>
2020-07-12 20:14:27 +0900

    0712comit

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <router-link to="/about">About</router-link>
       |
       <router-link to="/game">Game</router-link>
+      <router-link to="/score">Score</router-link>
     </div>
     <router-view/>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
   mutations: {
+    lose: state => state.result = 0,
+    win: state => state.result = 1,
+    draw: state => state.result = 2,
   },
   actions: {
   },
   modules: {
+    result:state => {return state.result}
   }
 })
